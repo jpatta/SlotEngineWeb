89d2ad6f4e605c0ed79b01c397113abf7a489a7f
Julius Patta <jpatta@digitalgamesystems.com>
2020-08-11 21:04:48 +0200

    revised to exclude games 9-20

--- rdgames2020april.js
+++ rdgames2020april.js
@@ -39,43 +39,58 @@ request2.onload = function() {
   if (request2.status >= 200 && request2.status < 400) {
     data2.response.result.gameServers.forEach(game => {
       if (data2.response.result.gameClients[game.idgame]) {
-        const card = document.createElement('div')
-        card.setAttribute('class', 'card')
-        container.appendChild(card)
+        switch (game.idgame) {
+          case "Game1_97_v1":
+            case "Game2_97_v1":
+              case "Game3_97_v1":
+                case "Game4_97_v1":
+                  case "Game5_97_v1":
+                    case "Game6_97_v1":
+                      case "Game7_97_v1":
+                        case "Game8_97_v1":
 
-        /*const h1 = document.createElement('h1')
-        h1.textContent = game.gameName
-        card.appendChild(h1)*/
+          const card = document.createElement('div')
+          card.setAttribute('class', 'card')
+          container.appendChild(card)
 
-        /*const p = document.createElement('p')
-        game.gameDescription = game.gameDescription.substring(0, 300)
-        p.textContent = `${game.gameDescription}...`
-        card.appendChild(p)*/
+          /*const h1 = document.createElement('h1')
+          h1.textContent = game.gameName
+          card.appendChild(h1)*/
 
-        const gameLogo = document.createElement('img')
-        gameLogo.src = data2.response.result.gameResourceServer + '/' + data2.response.result.gameClients[game.idgame].logo
-        gameLogo.style.maxWidth = "50%"
-        if (game.gameStatus == 1) {
-          gameLogo.onclick = function() { launchGame(this, data2.response.result.gameResourceServer + '/' + data2.response.result.gameClients[game.idgame].game) }
-        } else {
-          gameLogo.onclick = function() { alert ('Game is disabled')}
-          const h2 = document.createElement('h2')
-          h2.textContent = 'Game is Disabled'
-          card.appendChild(h2)
+          /*const p = document.createElement('p')
+          game.gameDescription = game.gameDescription.substring(0, 300)
+          p.textContent = `${game.gameDescription}...`
+          card.appendChild(p)*/
+
+          const gameLogo = document.createElement('img')
+          gameLogo.src = data2.response.result.gameResourceServer + '/' + data2.response.result.gameClients[game.idgame].logo
+          gameLogo.style.maxWidth = "50%"
+          if (game.gameStatus == 1) {
+            gameLogo.onclick = function() { launchGame(this, data2.response.result.gameResourceServer + '/' + data2.response.result.gameClients[game.idgame].game) }
+          } else {
+            gameLogo.onclick = function() { alert ('Game is disabled')}
+            const h2 = document.createElement('h2')
+            h2.textContent = 'Game is Disabled'
+            card.appendChild(h2)
+          }
+          card.appendChild(gameLogo)
+
+          const gameDesign = document.createElement('img')
+          var gameDesignPart = data2.response.result.gameClients[game.idgame].logo
+          gameDesignPart = gameDesignPart.substring(gameDesignPart.lastIndexOf("/")+1,gameDesignPart.lastIndexOf("_"))
+          gameDesign.src = data2.response.result.gameResourceServer + '/gameDesign/' + gameDesignPart + '.png'
+          if (game.gameStatus == 1) {
+            gameDesign.onclick = function() { launchGame(this, data2.response.result.gameResourceServer + '/' + data2.response.result.gameClients[game.idgame].game) }
+          } else {
+            gameDesign.onclick = function() { alert ('Game is disabled')}
+          }
+
+          card.appendChild(gameDesign)
+          break;
+        
+          default:
+            break;
         }
-        card.appendChild(gameLogo)
-
-        const gameDesign = document.createElement('img')
-        var gameDesignPart = data2.response.result.gameClients[game.idgame].logo
-        gameDesignPart = gameDesignPart.substring(gameDesignPart.lastIndexOf("/")+1,gameDesignPart.lastIndexOf("_"))
-        gameDesign.src = data2.response.result.gameResourceServer + '/gameDesign/' + gameDesignPart + '.png'
-        if (game.gameStatus == 1) {
-          gameDesign.onclick = function() { launchGame(this, data2.response.result.gameResourceServer + '/' + data2.response.result.gameClients[game.idgame].game) }
-        } else {
-          gameDesign.onclick = function() { alert ('Game is disabled')}
-        }
-
-        card.appendChild(gameDesign)
       }
     })
 
